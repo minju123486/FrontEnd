f2713f062983fd77b65843d79a1825acd0b4e1b8
minju123486 <minju123486@gmail.com>
2024-05-21 16:04:26 +0900

    blog update

--- Fanatic-React-Template/src/components/Elements/BlogBox.jsx
+++ Fanatic-React-Template/src/components/Elements/BlogBox.jsx
@@ -6,9 +6,8 @@ export default function BlogBox({ tag, title, text, action, author }) {
     <WrapperBtn className="animate pointer" onClick={action ? () => action() : null}>
       <Wrapper className="whiteBg radius8 shadow">
         <h3 className="font20 extraBold">{title}</h3>
-        <p className="font13" style={{ padding: "30px 0" }}>
-          {text}
-        </p>
+        <p className="font13" style={{ padding: "30px 0" }} dangerouslySetInnerHTML={{ __html: text }} />
+ 
         <p className="font13 extraBold">{author}</p>
         <div className="flex">
           <p className="tag coralBg radius6 font13 extraBold">{tag}</p>
